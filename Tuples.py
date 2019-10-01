0105cd0f1a86964300c740cb1c35e6385a4e7a4e
PyTuubi <jonni.liljamo@gmail.com>
2019-10-01 16:01:38 +0300

    New

---
+++ Tuples.py
@@ -0,0 +1,12 @@
+t = (1,2,3)
+
+# len() same as lists
+# can have different data types in one tuple i.e. t = (1,"two",3.0)
+# slicing and indexing is same
+
+t2 = ("a","a","b")
+
+#print(t2.count("a")) # how many times something is mentioned
+#print(t2.index("a")) # first time a certian thing appears
+
+# cant reassign stuff u know yes yes
