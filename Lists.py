d5d21b83f1ee768e5ed5d94bebd5494e731b03fc
PyTuubi <jonni.liljamo@gmail.com>
2019-10-01 14:39:19 +0300

    Update

--- Lists.py
+++ Lists.py
@@ -16,4 +16,22 @@ my_list2= ["string",1,2.1]
 
 my_list[0] = "ONE" # Edit lists easily
 
+my_list.append(4) # Add stuff to lists
+
+my_list.pop(1) # Remove from list
+
 #print(my_list)
+
+new_list = ["a","e","x","b","c"]
+num_list = [4,1,8,3]
+
+# Sort lists
+new_list.sort()
+num_list.sort()
+
+#print(new_list)
+#print(num_list)
+
+num_list.reverse() # Obvious
+
+print(num_list)
