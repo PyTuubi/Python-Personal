a18bdebdaf38863ffdce78184bdd1161b1176f9e
PyTuubi <jonni.liljamo@gmail.com>
2019-10-01 13:48:16 +0300

    Update

--- Lists.py
+++ Lists.py
@@ -6,6 +6,14 @@ my_list2= ["string",1,2.1]
 # Indexing and slicing
 
 #print(my_list2[0])
-print(my_list2[1:])
+#print(my_list2[1:])
 
+# same as strings
 
+#print(my_list + my_list2)
+
+# same as strings
+
+my_list[0] = "ONE" # Edit lists easily
+
+#print(my_list)
